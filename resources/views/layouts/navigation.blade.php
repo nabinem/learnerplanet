d2a3645555fd996eb0df043db45635cf5e611dab
Nabin Nembang <yakthumbanabinem@gmail.com>
2024-01-06 04:21:10 +0545

    init repo with simple designs

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <nav class="navbar navbar-expand-lg navigation-wrap">
+        <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/logo.png') }}" />
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="navbar-toggler-icon"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#explore-course">Courses</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#testomonials">Testimonial</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#faq">FAQ</a>
+              </li>
+              <li>
+                <button class="white-btn small-btn">LOG IN</button>
+              </li>
+              <li>
+                <button class="main-btn">SIGN UP</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</header>
