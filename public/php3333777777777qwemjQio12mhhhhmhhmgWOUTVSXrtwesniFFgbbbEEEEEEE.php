b72ef31a99aacfc3a690d82f156d2ecabd89b7b7
Nabin Nembang <yakthumbanabinem@gmail.com>
2024-01-11 05:13:19 +0545

    updates

---
+++ public/php3333777777777qwemjQio12mhhhhmhhmgWOUTVSXrtwesniFFgbbbEEEEEEE.php
@@ -0,0 +1,22 @@
+<?php
+
+    //$duration = 400;
+
+    //echo "running code for $duration seconds";
+    //set_time_limit($duration + 5);
+    //set_time_limit(0);
+    /*$a = time();
+    for($i=0;;$i++){
+        if(time() - $a > $duration){
+            break;
+        }   
+    }*/
+
+
+    /*
+    echo "sleeping for {$duration} seconds";
+    sleep($duration);
+    */
+    
+
+    echo phpinfo();
