de5ebcb751430722b94f17c006f126bad7a07687
Nabin Nembang <yakthumbanabinem@gmail.com>
2024-02-17 02:04:50 +0545

    different html5 players demos

---
+++ public/plugins/videojs-hls-quality-selector/CHANGELOG.md
@@ -0,0 +1,109 @@
+<a name="2.0.0"></a>
+# 2.0.0 (2023-11-22)
+
+### Features
+
+* **ci:** add circleci config ([aa7653c](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/aa7653c))
+* **config:** display resolutions in menu button ([d285eef](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/d285eef))
+
+### Bug Fixes
+
+* Hide levels with 0 or undefined height ([43a24b0](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/43a24b0))
+* **ci:** add vjs 7 to travis ci ([7d62e26](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/7d62e26))
+* **deps:** bump dependencies ([4980678](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/4980678))
+* **deps:** force update dot-prop ([4663a0c](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/4663a0c))
+* **deps:** update deps ([c69e167](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/c69e167))
+* **docs:** documentation formatting ([80d7bb3](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/80d7bb3))
+* **npm:** remove force resolutions ([ab34990](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/ab34990))
+* **npm:** update dependiencies, add CI deploy job ([2c67fbe](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/2c67fbe))
+* **plugin:** add a quality getter ([13b8be9](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/13b8be9))
+* **plugin:** fix merged code lint issues ([dba70d5](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/dba70d5))
+* add circleci status ([85ea4a9](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/85ea4a9))
+* add npm release badge ([81f1cff](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/81f1cff))
+* bump version ([a033124](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/a033124))
+* update readme to reflect vjs support ([16fdfa3](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/16fdfa3))
+* **npm:** update dependiencies, add CI deploy job ([738af51](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/738af51))
+* **plugin:** add additional component overrides ([c491236](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/c491236))
+* update to work with vjs >= 7 ([9711157](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/9711157))
+
+### Chores
+
+* **deps:** bump handlebars from 4.7.6 to 4.7.7 ([a4bcaed](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/a4bcaed))
+* **deps:** bump hosted-git-info from 2.6.0 to 2.8.9 ([0ef625d](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/0ef625d))
+* **deps:** bump ini from 1.3.5 to 1.3.8 ([3624c6c](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/3624c6c))
+* **deps:** bump lodash from 4.17.14 to 4.17.19 ([ecbcc28](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/ecbcc28))
+* **deps:** bump lodash from 4.17.19 to 4.17.21 ([c8e240e](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/c8e240e))
+* **deps:** bump lodash.template from 4.4.0 to 4.5.0 ([b559126](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/b559126))
+* **deps:** bump path-parse from 1.0.5 to 1.0.7 ([9971f34](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/9971f34))
+* **deps:** bump set-getter from 0.1.0 to 0.1.1 ([c8128ce](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/c8128ce))
+* **deps:** bump tar from 4.4.8 to 4.4.19 ([5e54755](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/5e54755))
+* **deps:** bump trim-off-newlines from 1.0.1 to 1.0.3 ([839fc17](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/839fc17))
+* **deps:** bump video.js from 7.6.0 to 7.14.3 ([896b5fe](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/896b5fe))
+* **deps-dev:** bump karma from 3.1.4 to 6.3.16 ([7ad3b8d](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/7ad3b8d))
+* **deps-dev:** bump node-sass from 4.14.1 to 7.0.0 ([a87846e](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/a87846e))
+* bump version ([2ecfa5f](https://github.com/jb-alvarado/videojs-hls-quality-selector/commit/2ecfa5f))
+
+<a name="1.1.4"></a>
+## [1.1.4](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v1.1.3...v1.1.4) (2020-10-07)
+
+### Bug Fixes
+
+* **npm:** remove force resolutions ([ab34990](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/ab34990))
+
+<a name="1.1.3"></a>
+## [1.1.3](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v1.1.2...v1.1.3) (2020-10-07)
+
+### Bug Fixes
+
+* **docs:** documentation formatting ([80d7bb3](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/80d7bb3))
+* **plugin:** add a quality getter ([13b8be9](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/13b8be9))
+
+<a name="1.1.2"></a>
+## [1.1.2](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v1.1.1...v1.1.2) (2020-09-03)
+
+### Bug Fixes
+
+* **deps:** force update dot-prop ([4663a0c](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/4663a0c))
+* **deps:** update deps ([c69e167](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/c69e167))
+
+### Chores
+
+* **deps:** bump lodash from 4.17.14 to 4.17.19 ([ecbcc28](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/ecbcc28))
+
+<a name="1.1.1"></a>
+## [1.1.1](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v1.1.0...v1.1.1) (2020-01-17)
+
+### Bug Fixes
+
+* **deps:** bump dependencies ([4980678](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/4980678))
+* **plugin:** fix merged code lint issues ([dba70d5](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/dba70d5))
+
+### Chores
+
+* **deps:** bump lodash.template from 4.4.0 to 4.5.0 ([b559126](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/b559126))
+
+<a name="1.1.0"></a>
+# [1.1.0](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v1.0.5...v1.1.0) (2019-10-11)
+
+### Features
+
+* **config:** display resolutions in menu button ([d285eef](https://github.com/chrisboustead/videojs-hls-quality-selector/commit/d285eef))
+
+<a name="0.0.8"></a>
+## [0.0.8](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v0.0.7...v0.0.8) (2018-06-27)
+
+<a name="0.0.7"></a>
+## [0.0.7](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v0.0.6...v0.0.7) (2018-06-05)
+
+<a name="0.0.6"></a>
+## [0.0.6](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v0.0.5...v0.0.6) (2018-06-05)
+
+<a name="0.0.5"></a>
+## [0.0.5](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v0.0.4...v0.0.5) (2018-06-05)
+
+<a name="0.0.4"></a>
+## [0.0.4](https://github.com/chrisboustead/videojs-hls-quality-selector/compare/v0.0.3...v0.0.4) (2018-04-27)
+
+<a name="0.0.3"></a>
+## 0.0.3 (2018-04-27)
+
--- resources/views/courses/add_edit_form.blade.php
+++ resources/views/courses/add_edit_form.blade.php
@@ -2,6 +2,8 @@
     
     @push('pluginCss')
         <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
+        <link href="https://unpkg.com/@silvermine/videojs-quality-selector/dist/css/quality-selector.css" rel="stylesheet">
+        <link href="{{ asset('plugins/videojs-hls-quality-selector/dist/videojs-hls-quality-selector.css') }}" rel="stylesheet">
     @endpush
 
     <div class="app-content-header">
@@ -254,10 +256,42 @@
 
                                                     <div class="row">
                                                         <div class="col-sm-10 mt-3 border py-4">
-                                                            <h5>HLS STREAMING VIDEOS TEST<hr/> </h5>
+                                                            <h5>HLS STREAMING VIDEOS(Plyr.io Hls.js Implementation)<hr/> </h5>
                                                             <video id="hlsPlayer" width="100%" class="" controls></video>
                                                         </div>
                                                     </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-10 mt-3 border py-4">
+                                                            <h5>VIDEO.JS plugin: @silvermine/videojs-quality-selector <hr/> </h5>
+                                                            <video id="videoJsQualitySelectorDemo" class="video-js vjs-default-skin" style="width: 854px; height: 480px;" controls>
+                                                                <source src="http://learnerplanet.local/VIDEOS_TEST/RESOLUTIONS/sample_1080p.mp4" label="1080p">
+                                                                <source src="http://learnerplanet.local/VIDEOS_TEST/RESOLUTIONS/sample_720p.mp4" label="720p">
+                                                                <source src="http://learnerplanet.local/VIDEOS_TEST/RESOLUTIONS/sample_480p.mp4" label="480p" selected="true">
+                                                                <source src="http://learnerplanet.local/VIDEOS_TEST/RESOLUTIONS/sample_240p.mp4" label="240p">
+                                                            </video>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-10 mt-3 border py-4">
+                                                            <h5>VIDEO.JS plugin: videojs-contrib-quality-levels(most downloaded)<hr/> </h5>
+                                                            <div id="fixture"></div>
+                                                            <div id="quality-levels">
+                                                                <h2>Quality Levels:</h2>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-10 mt-3 border py-4">
+                                                            <h5>VIDEO.JS plugin: jb-videojs-hls-quality-selector plugin <hr/> </h5>
+                                                            <video id="jbHlsQtSelectorPlayer" class="video-js vjs-default-skin" style="width: 854px; height: 480px;" controls>
+                                                                <source src="http://learnerplanet.local/VIDEOS_TEST/HLS/sample.m3u8">
+                                                            </video>
+                                                        </div>
+                                                    </div>
+
                                                     @if (!empty($course->trailer))
                                                         <div class="row">
                                                             <div class="col-sm-6 mt-3">
@@ -369,6 +403,8 @@
     @push('pluginScripts')
         <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
         <script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
+        <script src="https://unpkg.com/@silvermine/videojs-quality-selector/dist/js/silvermine-videojs-quality-selector.min.js"></script>
+       <script src="{{ asset('plugins/videojs-hls-quality-selector/dist/jb-videojs-hls-quality-selector.min.js') }}"></script>
     @endpush
 
     @push('scripts')
@@ -407,11 +443,14 @@
                     }
                 });
 
-                var video = document.getElementById('hlsPlayer');
+                //http://learnerplanet.local/VIDEOS_TEST/HLS/sample.m3u8
+                //http://learnerplanet.local/VIDEOS_TEST/HLS/480_out.m3u8
 
+                // --STARTS SCRIPT FOR -- HLS STREAMING VIDEOS (Plyr.io Hls.js Implementation) --
+                var video = document.getElementById('hlsPlayer');
                 if(Hls.isSupported()) {
                     var hls = new Hls();
-                    hls.loadSource('http://learnerplanet.local/HLS_SAMPLE_VIDEOS/HLS/sample.m3u8');
+                    hls.loadSource('http://learnerplanet.local/VIDEOS_TEST/HLS/sample.m3u8');
                     hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
                         //video.play();
                         const availableQualitites = hls.levels.map(i => i.height);
@@ -431,7 +470,7 @@
                             'settings', // Settings menu
                             'pip', // Picture-in-picture (currently Safari only)
                             'airplay', // Airplay (currently Safari only)
-                            'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+                            //'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
                             'fullscreen', // Toggle fullscreen
                         ];
                         defaultOptions.quality = {
@@ -455,11 +494,108 @@
                         }
                     });
                 }
+                // --ENDS SCRIPT FOR -- HLS STREAMING VIDEOS(Plyr.io Hls.js Implementation) --
 
-                //http://learnerplanet.local/HLS_SAMPLE_VIDEOS/HLS/sample.m3u8
-                //http://learnerplanet.local/HLS_SAMPLE_VIDEOS/HLS/480_out.m3u8
-                     
+                
+                // -- STARTS SCRIPT FOR -- VIDEO.JS plugin: @silvermine/videojs-quality-selector plugin --
+                videojs('videoJsQualitySelectorDemo', {}, function() {
+                    var player = this;
 
+                    player.controlBar.addChild('QualitySelector');
+                });
+                // -- ENDS SCRIPT FOR -- VIDEO.JS plugin: @silvermine/videojs-quality-selector plugin --
+
+                // -- STARTS SCRIPT FOR -- VIDEO.JS plugin: videojs-contrib-quality-levels --
+                function createQualityButton(qualityLevel, parent) {
+                    var button = document.createElement('button');
+                    var classes = button.classList;
+
+                    if (qualityLevel.enabled) {
+                        classes.add('enabled');
+                    } else {
+                        classes.add('disabled');
+                    }
+
+                    button.innerHTML = qualityLevel.id + ': ' + qualityLevel.bitrate + ' kbps';
+                    button.id = 'quality-level-' + qualityLevel.id;
+                    button.type = 'button';
+
+                    button.onclick = function() {
+                        var old = qualityLevel.enabled;
+                        qualityLevel.enabled = !old;
+                        button.classList.toggle('enabled');
+                        button.classList.toggle('disabled');
+                    }
+                    parent.appendChild(button);
+                }
+
+                function createPlayer(callback) {
+                    var video = document.createElement('video');
+                    video.id = 'videojs-contrib-quality-levels-player';
+                    video.className = 'video-js vjs-default-skin';
+                    video.setAttribute('controls', true);
+                    video.setAttribute('height', 300);
+                    video.setAttribute('width', 600);
+                    document.querySelector('#fixture').appendChild(video);
+
+                    var options = {
+                        autoplay: false,
+                        qualityLevels: {}
+                    };
+                    var url = 'http://learnerplanet.local/VIDEOS_TEST/HLS/sample.m3u8';
+                    var type = 'application/x-mpegURL';
+
+                    try {
+                        window.player = videojs(video.id, options);
+
+                        window.player.src({
+                            src: url,
+                            type: type
+                        });
+
+                        callback(window.player);
+                    } catch(err) {
+                        console.log("caught an error trying to create and add src to player:", err);
+                    }
+                }
+                function setup(player) {
+                    player.ready(function() {
+                        var qualityLevels = player.qualityLevels();
+                        var container = document.getElementById('quality-levels');
+
+                        qualityLevels.on('addqualitylevel', function(event) {
+                            createQualityButton(event.qualityLevel, container);
+                        });
+
+                        qualityLevels.on('change', function(event) {
+                            for (var i = 0; i < qualityLevels.length; i++) {
+                                var level = qualityLevels[i];
+                                var button = document.getElementById('quality-level-' + level.id);
+
+                                button.classList.remove('selected');
+                            }
+
+                            var selected = qualityLevels[event.selectedIndex];
+                            var button = document.getElementById('quality-level-' + selected.id);
+                            button.classList.add('selected');
+                        })
+                    });
+                }
+
+                (function(window, videojs) {
+                    createPlayer(setup);
+                })(window, window.videojs);
+                // -- ENDS SCRIPT FOR - VIDEO.JS plugin: videojs-contrib-quality-levels --
+                
+                // -- STARTS SCRIPT FOR - VIDEO.JS plugin: jb-videojs-hls-quality-selector --
+                var jbHlsPlayer = videojs('jbHlsQtSelectorPlayer');
+                //jbHlsPlayer.controlBar.removeChild('settingsButton');
+                jbHlsPlayer.hlsQualitySelector({
+                    displayCurrentQuality: true,
+                    defaultQuality: '720p'
+                });
+                // -- ENDS SCRIPT FOR - VIDEO.JS plugin: jb-videojs-hls-quality-selector --
+                
             });
         </script>
     @endpush
