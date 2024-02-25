1dd5d60530b4d5ec2da8959a36da8aa1fcd64c03
Nabin Nembang <yakthumbanabinem@gmail.com>
2024-02-25 20:33:27 +0545

    default admin user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@learnerplanet.com',
+            'password' => Hash::make('admin@lp0923'),
+            'role' => 'admin'
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
